6eb10507fd9cd04d730587cec23aaf13e24600a9
nirgluzman <nir.gluzman@gmail.com>
2023-03-16 10:12:34 +0100

    add requests.rest to send HTTP requets directly from VS code

--- server.js
+++ server.js
@@ -6,8 +6,23 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+const posts = [
+  {
+    username: "Nir",
+    title: "Post-1",
+  },
+  {
+    username: "Idan",
+    title: "Post-2",
+  },
+];
+
 app.get("/", (req, res) => {
   res.send("Welcome to JWT AuthN Tutorial !");
+});
+
+app.get("/posts", (req, res) => {
+  res.json(posts);
 });
 
 app.listen(PORT, () => {
